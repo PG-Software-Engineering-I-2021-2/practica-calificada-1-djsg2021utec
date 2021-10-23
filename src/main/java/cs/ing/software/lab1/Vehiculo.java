7611098b63e22044426c1727960e3f766b25d81f
Dan Joshua Santivañez Gutarra <83432705+djsg2021utec@users.noreply.github.com>
2021-10-23 16:56:41 -0500

    actualizar

---
+++ src/main/java/cs/ing/software/lab1/Vehiculo.java
@@ -0,0 +1,34 @@
+package cs.ing.software.lab1;
+import java.text.DecimalFormat;
+
+public class Vehiculo {
+    double  cantidad;
+    double capacidad;
+    double consumo;
+
+    Vehiculo(double prCantidad, double prConsumo, double prCapacidad){
+        this.cantidad = prCantidad;
+        this.consumo = prConsumo;
+        this.capacidad = prCapacidad;
+    }
+
+    String conduccion(double prkm){
+        DecimalFormat df = new DecimalFormat("0.00");
+        double fuelRequiere = prkm*this.consumo;
+        if(cantidad-fuelRequiere<0){
+            return ("Vehiculo necesita reabastecimiento de combustible");
+        }
+        else{
+            this.cantidad -=fuelRequiere;
+            return ("Vehiculo viajo " +(prkm)+" km y aun tiene " +df.format(cantidad)+ " de combustible");
+        }
+    }
+}
+
+
+
+
+
+
+
+
